ec70239e38b1e4f6547ff0c1e7ecf566ebf1ada6
gdumarchat <gurvandumarchat@gmail.com>
2024-08-23 17:28:53 +0200

    header component done

---
+++ components/Header.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View } from "react-native";
+import { defaultStyles } from "../App";
+
+export const Header = () => {
+  return (
+    <View style={styles.header}>
+      <Text style={[defaultStyles.white, defaultStyles.heading2, styles.title]}>
+        Voyag'r
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  header: {
+    width: "100%",
+    paddingTop: 40,
+    paddingVertical: 16,
+    backgroundColor: "#09A8D4",
+  },
+  title: {
+    width: "100%",
+    textAlign: "center",
+  },
+});
